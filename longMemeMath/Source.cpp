ce4021b0f3347b759625049db1d24c3b5d21b8a6
Volodya <afork4ik@gmail.com>
2020-04-25 18:33:19 +1000

    gooooooooooooooose

--- longMemeMath/Source.cpp
+++ longMemeMath/Source.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <regex>
 #include <vector>
+#include <ctime>
+#include <cstdio>
 using namespace std;
 class math
 {
@@ -29,7 +31,6 @@ private:
 			cout << "Answer: ";
 			for (int a = 0; a < num1.size(); a++)
 				cout << num1[a];
-			cout << endl << endl;
 		}
 		else
 		{
@@ -55,7 +56,6 @@ private:
 			cout << "Answer: ";
 			for (int a = 0; a < num1.size(); a++)
 				cout << num1[a];
-			cout << endl << endl;
 		}
 	}
 	void minus(vector<int> num1, vector<int> num2)
@@ -63,8 +63,8 @@ private:
 		if (num1.size() > num2.size())
 		{
 			int temp = num1.size() - 1;
-			for (int a = num2.size(); a > 0; a--)
-			{
+			for (int a = num2.size(); a > 0; a--) // >0)
+			{                                     // (_>  goooooooosePOWEEEEEEEEEEEEEEEEEEEEER
 				num1[temp] -= num2[a - 1];
 				if (num1[temp] < 0)
 				{
@@ -76,7 +76,6 @@ private:
 			cout << "Answer: ";
 			for (int a = 0; a < num1.size(); a++)
 					cout << num1[a];
-			cout << endl << endl;
 		}
 		else if (num1.size() < num2.size())
 		{
@@ -95,9 +94,25 @@ private:
 				temp--;
 			}
 			cout << "Answer: -";
+			int i = 0;
+			while (1)
+			{
+				if (num1[i] == 0)
+					num1[i] = ' ';
+				else
+					break;
+				i++;
+
+			}
 			for (int a = 0; a < num1.size(); a++)
-				cout << num1[a];
-			cout << endl << endl;
+			{
+				if (num1[a] == ' ')
+				{
+					cout << "";
+				}
+				else
+					cout << num1[a];
+			}
 		}
 		else if (num2.size() == num2.size())
 		{
@@ -123,16 +138,36 @@ private:
 						num1[temp] -= num2[a - 1];
 						if (num1[temp] < 0)
 						{
-							num1[temp] += 10;
-							num1[temp - 1]--;
+							if (temp == 0)
+								cout << "";
+							else
+							{
+								num1[temp] += 10;
+								num1[temp - 1]--;
+							}
 						}
 						temp--;
 					}
 					cout << "Answer: -";
+					int i = 0;
+					while (1)
+					{
+						if (num1[i] == 0)
+							num1[i] = ' ';
+						else
+							break;
+						i++;
+
+					}
 					for (int a = 0; a < num1.size(); a++)
-						cout << num1[a];
-					cout << endl << endl;
-					break;
+					{
+						if (num1[a] == ' ')
+						{
+							cout << "";
+						}
+						else
+							cout << num1[a];
+					}
 				}
 				else if (num1[a] > num2[a])
 				{
@@ -148,10 +183,25 @@ private:
 						temp--;
 					}
 					cout << "Answer: ";
+					int i = 0;
+					while (1)
+					{
+						if (num1[i] == 0)
+							num1[i] = ' ';
+						else
+							break;
+						i++;
+
+					}
 					for (int a = 0; a < num1.size(); a++)
-						cout << num1[a];
-					cout << endl << endl;
-					break;
+					{
+						if (num1[a] == ' ')
+						{
+							cout << "";
+						}
+						else
+							cout << num1[a];
+					}
 				}
 			}
 		}
@@ -233,7 +283,7 @@ private:
 			else
 				break;
 			i++;
-			
+
 		}
 		for (int a = 0; a < res.size(); a++)
 		{
@@ -244,11 +294,179 @@ private:
 			else
 				cout << res[a];
 		}
-		cout << endl << endl;
 	}
-	void divison(vector<int> num1, vector<int> num2A)
+	void divison(vector<int> num1, vector<int> num2)
 	{
-
+		if (num1.size() > num2.size())
+		{
+			vector<int> tempNum2;
+			for (int a = 0; a < num1.size(); a++)
+				tempNum2.push_back(0);
+			int temp = tempNum2.size() - 1;
+			for (int a = num2.size() - 1; a >= 0; a--)
+			{
+				tempNum2[temp] += num2[a];
+				temp--;
+			}
+			bool trigger = false;
+			int count = 0;
+			vector<int> saveNum2;
+			for (int a = 0; a < tempNum2.size(); a++)
+			{
+				saveNum2.push_back(0);
+			}
+			for (int a = 0; a < tempNum2.size(); a++)
+			{
+				saveNum2[a] += tempNum2[a];
+			}
+			while (1)
+			{
+				for (int a = 0; a < num1.size(); a++)
+				{
+					if (num1[a] == tempNum2[a])
+					{
+						if (a == num1.size())
+						{
+							count++;
+							trigger = true;
+							break;
+						}
+						continue;
+					}
+					else if (num1[a] < tempNum2[a])
+					{
+						trigger = true;
+						break;
+					}
+					else if (num1[a] > tempNum2[a])
+					{
+						trigger = false;
+						break;
+					}
+				}
+				if (trigger == true)
+					break;
+				else if (trigger == false)
+				{
+					count++;
+					for (int i = tempNum2.size() - 1; i >= 0; i--)
+					{
+						tempNum2[i] += saveNum2[i];
+						if (tempNum2[i] >= 10)
+						{
+							if (i == 0)
+								cout << "";
+							else
+							{
+								tempNum2[i] -= 10;
+								tempNum2[i - 1]++;
+							}
+						}
+					}
+				}
+			}
+			cout << "Answer: " << count << endl;
+		}
+		else if (num1.size() == num2.size())
+		{
+			bool trigger = false;
+			int count = 0;
+			if (num1.size() == 1)
+			{
+				count = num1[0] / num2[0];
+			}
+			else
+			{
+				vector<int> saveNum2;
+				for (int a = 0; a < num2.size(); a++)
+				{
+					saveNum2.push_back(0);
+				}
+				for (int a = 0; a < num2.size(); a++)
+				{
+					saveNum2[a] += num2[a];
+				}
+				while (1)
+				{
+					for (int a = 0; a < num1.size(); a++)
+					{
+						if (num1[a] == num2[a])
+						{
+							if (a == num1.size() - 1)
+							{
+								count++;
+								trigger = true;
+								break;
+							}
+							continue;
+						}
+						else if (num1[a] < num2[a])
+						{
+							trigger = true;
+							break;
+						}
+						else if (num1[a] > num2[a])
+						{
+							trigger = false;
+							break;
+						}
+					}
+					if (trigger == true)
+						break;
+					else if (trigger == false)
+					{
+						count++;
+						for (int i = num2.size() - 1; i >= 0; i--)
+						{
+							num2[i] += saveNum2[i];
+							if (num2[i] >= 10)
+							{
+								if (i == 0)
+									cout << "";
+								else
+								{
+									num2[i] -= 10;
+									num2[i - 1]++;
+								}
+							}
+						}
+					}
+				}
+			}
+			cout << "Answer: " << count << endl;
+		}
+		else if (num1.size() < num2.size())
+		{
+			int zero = -1;
+			while (1)
+			{
+				if (num1.size() == num2.size())
+					break;
+				num1.push_back(0);
+				zero++;
+			}
+			int count = 0;
+			for (int a = 0; a < num1.size(); a++)
+			{
+				if (num1[a] == num2[a])
+				{
+					if (a = num1.size() - 1)
+					{
+						cout << "Answer: 0,";
+						for (int a = 0; a < zero; a++)
+							cout << "0";
+						cout << count;
+						break;
+					}
+					else
+					{
+						cout << "Answer: 0" << endl;
+						break;
+					}
+				}
+				
+			}
+		}
 	}
 public:
 	void conver(string num1, string num2, int option)
@@ -285,9 +503,22 @@ int main()
 	while (1)
 	{
 		math math;
-		cout << "Enter first number: " << endl;
+		int option;
+		cout << "Choose your destiny: " << endl;
+		cout << "1. +" << endl << "2. -" << endl << "3. *" << endl << "4. /" << endl << "5. For exit" << endl;
+		cin >> option;
+		if (option == 5)
+			return 0;
+		else if (option != 1 && option != 2 && option !=3 && option != 4)
+		{
+			cout << "Incorrect option" << endl;
+			system("pause");
+			break;
+		}
+		cout << "Enter first number: ";
 		string num1;
 		cin >> num1;
+		cout << endl;
 		cmatch check;
 		regex reg("[\\D]");
 		if (regex_search(num1.c_str(), check, reg))
@@ -311,12 +542,13 @@ int main()
 		}
 		if (i == 0)
 		{
-			cout << "Try again" << endl;
+			cout << "Zero cant be first number. Try again" << endl;
 			continue;
 		}
-		cout << "Enter second number: " << endl;
+		cout << "Enter second number: ";
 		string num2;
 		cin >> num2;
+		cout << endl;
 		temp = num2[0];
 		try
 		{
@@ -330,7 +562,7 @@ int main()
 		}
 		if (i == 0)
 		{
-			cout << "Try again" << endl;
+			cout << "Zero cant be first number. Try again" << endl;
 			continue;
 		}
 		if (regex_search(num2.c_str(), check, reg))
@@ -339,20 +571,10 @@ int main()
 			system("pause");
 			break;
 		}
-		int option;
-		cout << "Choose your destiny: " << endl;
-		cout << "1. +" << endl << "2. -" << endl << "3. *" << endl << "4. /" << endl << "5. For exit"<< endl;
-		cin >> option;
+		double start = clock();
 		if (option == 1 || option == 2 || option == 3 || option == 4)
 			math.conver(num1, num2, option);
-		else if (option == 5)
-			return 0;
-		else
-		{
-			cout << "Incorrect option" << endl;
-			system("pause");
-			continue;
-		}
+		cout << endl << "Time: " << (clock() - start) / CLOCKS_PER_SEC << endl << endl;
 	}
 	return 0;
 }
