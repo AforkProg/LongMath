daf4d5fb7ea20123714c442ec8adb7f5e4cea7d9
Volodya <afork4ik@gmail.com>
2020-04-25 21:32:04 +1000

    final

--- longMemeMath/Source.cpp
+++ longMemeMath/Source.cpp
@@ -208,191 +208,137 @@ private:
 	}
 	void multi(vector<int> num1, vector<int> num2)
 	{
-		vector<int> res;
-		for (int a = 0; a < num1.size() + num2.size() + 5; a++)
+		if (num1[0] == 0 || num2[0] == 0)
+			cout << "Answer: 0" << endl;
+		else
 		{
-			res.push_back(0);
-		}
-		if (num1.size() >= num2.size())
-		{
-			int a = 1;
-			int temp = res.size() - a;
-			for (int n2 = num2.size() - 1; n2 >= 0; n2--)
+			vector<int> res;
+			for (int a = 0; a < num1.size() + num2.size() + 5; a++)
 			{
-				for (int n1 = num1.size() - 1; n1 >= 0; n1--)
-				{
-					res[temp] += num1[n1] * num2[n2];
-					if (res[temp] >= 10)
-					{
-						for (int a = 0; ; a += 10)
-						{
-							if (a > res[temp])
-							{
-								a -= 10;
-								res[temp] -= a;
-								res[temp - 1] += a / 10;
-								break;
-							}
-							continue;
-						}
-					}
-					temp--;
-				}
-				a++;
-				temp = res.size() - a;
+				res.push_back(0);
 			}
-		}
-		if (num1.size() < num2.size())
-		{
-			vector<int> tempArray = num1;
-			num1 = num2;
-			num2 = tempArray;
-			int a = 1;
-			int temp = res.size() - a;
-			for (int n2 = num2.size() - 1; n2 >= 0; n2--)
+			if (num1.size() >= num2.size())
 			{
-				for (int n1 = num1.size() - 1; n1 >= 0; n1--)
+				int a = 1;
+				int temp = res.size() - a;
+				for (int n2 = num2.size() - 1; n2 >= 0; n2--)
 				{
-					res[temp] += num1[n1] * num2[n2];
-					if (res[temp] >= 10)
+					for (int n1 = num1.size() - 1; n1 >= 0; n1--)
 					{
-						for (int a = 0; ; a += 10)
+						res[temp] += num1[n1] * num2[n2];
+						if (res[temp] >= 10)
 						{
-							if (a > res[temp])
+							for (int a = 0; ; a += 10)
 							{
-								a -= 10;
-								res[temp] -= a;
-								res[temp - 1] += a / 10;
-								break;
+								if (a > res[temp])
+								{
+									a -= 10;
+									res[temp] -= a;
+									res[temp - 1] += a / 10;
+									break;
+								}
+								continue;
 							}
-							continue;
 						}
+						temp--;
 					}
-					temp--;
+					a++;
+					temp = res.size() - a;
 				}
-				a++;
-				temp = res.size() - a;
 			}
-		}
-		cout << "Answer: ";
-		int i = 0;
-		while (1)
-		{
-			if (res[i] == 0)
-				res[i] = ' ';
-			else
-				break;
-			i++;
+			if (num1.size() < num2.size())
+			{
+				vector<int> tempArray = num1;
+				num1 = num2;
+				num2 = tempArray;
+				int a = 1;
+				int temp = res.size() - a;
+				for (int n2 = num2.size() - 1; n2 >= 0; n2--)
+				{
+					for (int n1 = num1.size() - 1; n1 >= 0; n1--)
+					{
+						res[temp] += num1[n1] * num2[n2];
+						if (res[temp] >= 10)
+						{
+							for (int a = 0; ; a += 10)
+							{
+								if (a > res[temp])
+								{
+									a -= 10;
+									res[temp] -= a;
+									res[temp - 1] += a / 10;
+									break;
+								}
+								continue;
+							}
+						}
+						temp--;
+					}
+					a++;
+					temp = res.size() - a;
+				}
+			}
+			cout << "Answer: ";
+			int i = 0;
+			while (1)
+			{
+				if (res[i] == 0)
+					res[i] = ' ';
+				else
+					break;
+				i++;
 
-		}
-		for (int a = 0; a < res.size(); a++)
-		{
-			if (res[a] == ' ')
-			{
-				cout << "";
 			}
-			else
-				cout << res[a];
+			for (int a = 0; a < res.size(); a++)
+			{
+				if (res[a] == ' ')
+				{
+					cout << "";
+				}
+				else
+					cout << res[a];
+			}
 		}
 	}
 	void divison(vector<int> num1, vector<int> num2)
 	{
-		if (num1.size() > num2.size())
+		if (num2[0] == 0)
 		{
-			vector<int> tempNum2;
-			for (int a = 0; a < num1.size(); a++)
-				tempNum2.push_back(0);
-			int temp = tempNum2.size() - 1;
-			for (int a = num2.size() - 1; a >= 0; a--)
-			{
-				tempNum2[temp] += num2[a];
-				temp--;
-			}
-			bool trigger = false;
-			int count = 0;
-			vector<int> saveNum2;
-			for (int a = 0; a < tempNum2.size(); a++)
-			{
-				saveNum2.push_back(0);
-			}
-			for (int a = 0; a < tempNum2.size(); a++)
-			{
-				saveNum2[a] += tempNum2[a];
-			}
-			while (1)
-			{
-				for (int a = 0; a < num1.size(); a++)
-				{
-					if (num1[a] == tempNum2[a])
-					{
-						if (a == num1.size())
-						{
-							count++;
-							trigger = true;
-							break;
-						}
-						continue;
-					}
-					else if (num1[a] < tempNum2[a])
-					{
-						trigger = true;
-						break;
-					}
-					else if (num1[a] > tempNum2[a])
-					{
-						trigger = false;
-						break;
-					}
-				}
-				if (trigger == true)
-					break;
-				else if (trigger == false)
-				{
-					count++;
-					for (int i = tempNum2.size() - 1; i >= 0; i--)
-					{
-						tempNum2[i] += saveNum2[i];
-						if (tempNum2[i] >= 10)
-						{
-							if (i == 0)
-								cout << "";
-							else
-							{
-								tempNum2[i] -= 10;
-								tempNum2[i - 1]++;
-							}
-						}
-					}
-				}
-			}
-			cout << "Answer: " << count << endl;
+			cout << "Cannot be devided by zero" << endl;
 		}
-		else if (num1.size() == num2.size())
+		else if (num1[0] == 0)
+			cout << "Answer: 0" << endl;
+		else
 		{
-			bool trigger = false;
-			int count = 0;
-			if (num1.size() == 1)
+			if (num1.size() > num2.size())
 			{
-				count = num1[0] / num2[0];
-			}
-			else
-			{
+				vector<int> tempNum2;
+				for (int a = 0; a < num1.size(); a++)
+					tempNum2.push_back(0);
+				int temp = tempNum2.size() - 1;
+				for (int a = num2.size() - 1; a >= 0; a--)
+				{
+					tempNum2[temp] += num2[a];
+					temp--;
+				}
+				bool trigger = false;
+				int count = 0;
 				vector<int> saveNum2;
-				for (int a = 0; a < num2.size(); a++)
+				for (int a = 0; a < tempNum2.size(); a++)
 				{
 					saveNum2.push_back(0);
 				}
-				for (int a = 0; a < num2.size(); a++)
+				for (int a = 0; a < tempNum2.size(); a++)
 				{
-					saveNum2[a] += num2[a];
+					saveNum2[a] += tempNum2[a];
 				}
 				while (1)
 				{
 					for (int a = 0; a < num1.size(); a++)
 					{
-						if (num1[a] == num2[a])
+						if (num1[a] == tempNum2[a])
 						{
-							if (a == num1.size() - 1)
+							if (a == num1.size())
 							{
 								count++;
 								trigger = true;
@@ -400,12 +346,12 @@ private:
 							}
 							continue;
 						}
-						else if (num1[a] < num2[a])
+						else if (num1[a] < tempNum2[a])
 						{
 							trigger = true;
 							break;
 						}
-						else if (num1[a] > num2[a])
+						else if (num1[a] > tempNum2[a])
 						{
 							trigger = false;
 							break;
@@ -416,55 +362,95 @@ private:
 					else if (trigger == false)
 					{
 						count++;
-						for (int i = num2.size() - 1; i >= 0; i--)
+						for (int i = tempNum2.size() - 1; i >= 0; i--)
 						{
-							num2[i] += saveNum2[i];
-							if (num2[i] >= 10)
+							tempNum2[i] += saveNum2[i];
+							if (tempNum2[i] >= 10)
 							{
 								if (i == 0)
 									cout << "";
 								else
 								{
-									num2[i] -= 10;
-									num2[i - 1]++;
+									tempNum2[i] -= 10;
+									tempNum2[i - 1]++;
 								}
 							}
 						}
 					}
 				}
+				cout << "Answer: " << count << endl;
 			}
-			cout << "Answer: " << count << endl;
-		}
-		else if (num1.size() < num2.size())
-		{
-			int zero = -1;
-			while (1)
+			else if (num1.size() == num2.size())
 			{
-				if (num1.size() == num2.size())
-					break;
-				num1.push_back(0);
-				zero++;
-			}
-			int count = 0;
-			for (int a = 0; a < num1.size(); a++)
-			{
-				if (num1[a] == num2[a])
+				bool trigger = false;
+				int count = 0;
+				if (num1.size() == 1)
 				{
-					if (a = num1.size() - 1)
+					count = num1[0] / num2[0];
+				}
+				else
+				{
+					vector<int> saveNum2;
+					for (int a = 0; a < num2.size(); a++)
 					{
-						cout << "Answer: 0,";
-						for (int a = 0; a < zero; a++)
-							cout << "0";
-						cout << count;
-						break;
+						saveNum2.push_back(0);
 					}
-					else
+					for (int a = 0; a < num2.size(); a++)
 					{
-						cout << "Answer: 0" << endl;
-						break;
+						saveNum2[a] += num2[a];
+					}
+					while (1)
+					{
+						for (int a = 0; a < num1.size(); a++)
+						{
+							if (num1[a] == num2[a])
+							{
+								if (a == num1.size() - 1)
+								{
+									count++;
+									trigger = true;
+									break;
+								}
+								continue;
+							}
+							else if (num1[a] < num2[a])
+							{
+								trigger = true;
+								break;
+							}
+							else if (num1[a] > num2[a])
+							{
+								trigger = false;
+								break;
+							}
+						}
+						if (trigger == true)
+							break;
+						else if (trigger == false)
+						{
+							count++;
+							for (int i = num2.size() - 1; i >= 0; i--)
+							{
+								num2[i] += saveNum2[i];
+								if (num2[i] >= 10)
+								{
+									if (i == 0)
+										cout << "";
+									else
+									{
+										num2[i] -= 10;
+										num2[i - 1]++;
+									}
+								}
+							}
+						}
 					}
 				}
-				
+				cout << "Answer: " << count << endl;
+			}
+			else if (num1.size() < num2.size())
+			{
+				cout << "Answer: 0";
 			}
 		}
 	}
@@ -540,10 +526,13 @@ int main()
 			system("pause");
 			break;
 		}
-		if (i == 0)
+		if (num1.size() != 1)
 		{
-			cout << "Zero cant be first number. Try again" << endl;
-			continue;
+			if (i == 0)
+			{
+				cout << "Zero cant be first number. Try again" << endl;
+				continue;
+			}
 		}
 		cout << "Enter second number: ";
 		string num2;
@@ -560,10 +549,13 @@ int main()
 			system("pause");
 			break;
 		}
-		if (i == 0)
+		if (num2.size() != 1)
 		{
-			cout << "Zero cant be first number. Try again" << endl;
-			continue;
+			if (i == 0)
+			{
+				cout << "Zero cant be first number. Try again" << endl;
+				continue;
+			}
 		}
 		if (regex_search(num2.c_str(), check, reg))
 		{
